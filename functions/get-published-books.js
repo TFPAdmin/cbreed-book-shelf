a8e0c6cd3b63c27d8eb0d3fddc48f8f87e57202d
Corey H <github@thefamilyplan.xyz>
2025-07-14 22:42:45 -0800

    Create get-published-books.js

---
+++ functions/get-published-books.js
@@ -0,0 +1,9 @@
+export async function onRequest({ env }) {
+  try {
+    const stmt = env.DB.prepare("SELECT * FROM books WHERE published = 1 ORDER BY id DESC");
+    const books = await stmt.all();
+    return Response.json(books.results);
+  } catch (err) {
+    return new Response("Failed to get published books", { status: 500 });
+  }
+}
